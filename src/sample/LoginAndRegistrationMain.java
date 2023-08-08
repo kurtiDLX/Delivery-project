1e65c27826fc46089c6b49d95951dab38c1293cc
KurtiDLX <marikgames12@gmail.com>
2023-08-09 02:42:54 +0300

    - register gui - switching from the main window to the registration window

--- src/sample/LoginAndRegistrationMain.java
+++ src/sample/LoginAndRegistrationMain.java
@@ -1,11 +1,16 @@
 package sample;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class LoginAndRegistrationMain {
 
@@ -32,7 +37,20 @@ public class LoginAndRegistrationMain {
 
     @FXML
     void initialize() {
+        registrationButton.setOnAction(actionEvent -> {
+            try {
+                registrationButton.getScene().getWindow().hide();
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/sample/register.fxml"));
+                Parent root = loader.load();
+                Scene scene = new Scene(root);
+                Stage stage = (Stage) registrationButton.getScene().getWindow();
+                stage.setScene(scene);
+                stage.show();
 
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        });
     }
 
 }
---
+++ src/sample/Registration.java
@@ -0,0 +1,42 @@
+package sample;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class Registration {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private AnchorPane backgroundOwn;
+
+    @FXML
+    private Button confirmButton;
+
+    @FXML
+    private TextField newUserEmail;
+
+    @FXML
+    private TextField newUserName;
+
+    @FXML
+    private TextField newUserPassword;
+
+    @FXML
+    private TextField newUserPhone;
+
+    @FXML
+    void initialize() {
+
+
+    }
+
+}
