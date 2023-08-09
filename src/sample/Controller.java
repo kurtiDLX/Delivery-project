f30bf3d86239ae49066c4d9cefafd86cc5cd30c7
KurtiDLX <marikgames12@gmail.com>
2023-08-09 04:20:16 +0300

    - added an info button and a page for it

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -22,20 +22,24 @@ public class Controller {
     private URL location;
 
     @FXML
-    private ImageView backgroundImg;
-
-    @FXML
     private AnchorPane backgroundOwn;
 
     @FXML
     private ImageView imgProfile;
 
     @FXML
+    private Button infoButton;
+
+    @FXML
     private Button profileBut;
+
+    @FXML
+    private ImageView infoImg;
 
     @FXML
     void initialize() {
         profileBut.setGraphic(imgProfile);
+        infoButton.setGraphic(infoImg);
         profileBut.setOnAction(actionEvent -> {
           try {
               profileBut.getScene().getWindow().hide();
@@ -51,6 +55,19 @@ public class Controller {
           }
         });
 
+        infoButton.setOnAction(actionEvent -> {
+            try {
+                infoButton.getScene().getWindow().hide();
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("info.fxml"));
+                Parent root = loader.load();
+                Scene scene = new Scene(root);
+                Stage stage = new Stage();
+                stage.setScene(scene);
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
 }
--- src/sample/MainUserProfile.java
+++ src/sample/MainUserProfile.java
@@ -1,16 +1,22 @@
 package sample;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class MainUserProfile {
 
     static String userInfoEmail,userInfoLogin,userInfoPhone, userInfoPassword;
+
 
     @FXML
     private ResourceBundle resources;
@@ -23,6 +29,9 @@ public class MainUserProfile {
 
     @FXML
     private ImageView imgProfile;
+
+    @FXML
+    private Button infoButton;
 
     @FXML
     private ImageView profileAvatarId;
@@ -40,12 +49,26 @@ public class MainUserProfile {
     private Text userPhoneInfo;
 
     @FXML
+    private ImageView infoImg;
+
+    @FXML
     void initialize() {
-    profileLogin.setText(userInfoLogin);
-    userPhoneInfo.setText(userInfoPhone);
-    userEmail.setText(userInfoEmail);
-
-
+        profileLogin.setText(userInfoLogin);
+        userPhoneInfo.setText(userInfoPhone);
+        userEmail.setText(userInfoEmail);
+        infoButton.setOnAction(actionEvent -> {
+            try {
+                infoButton.getScene().getWindow().hide();
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("info.fxml"));
+                Parent root = loader.load();
+                Scene scene = new Scene(root);
+                Stage stage = new Stage();
+                stage.setScene(scene);
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
 
